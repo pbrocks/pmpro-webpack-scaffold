0bc588340b2c996e7e12fa9532dacd8f48649b89
PBrocks <paul@barthmaier.rocks>
2018-06-13 11:26:44 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+console.log('PMPro Webpack Server test successful!');
+
+html = '<img src="images/pmpro-logo.png" /><h1>Hello PMPro!!</h1><h3>from Webpack Dev Server</h3><p>Great!! It looks like you have run YARN!!</p>';
+document.getElementById("homepage").innerHTML = html;
